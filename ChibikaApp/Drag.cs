6a26ec06deac33a4d4ff8c8daba02e0ef24ccf22
SAn4Es_TV <sanes328@gmail.com>
2022-01-24 12:31:20 +0200

    Добавьте файлы проекта.

---
+++ ChibikaApp/Drag.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows;
+
+namespace ChibikaApp
+{
+    public class Drag
+    {
+        public static readonly DependencyProperty EnableDragProperty = DependencyProperty.RegisterAttached(
+            "EnableDrag",
+            typeof(bool),
+            typeof(Drag),
+            new PropertyMetadata(default(bool), OnLoaded));
+        private static void OnLoaded(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var uiElement = dependencyObject as UIElement;
+            if (uiElement == null || (dependencyPropertyChangedEventArgs.NewValue is bool) == false)
+            {
+                return;
+            }
+            if ((bool)dependencyPropertyChangedEventArgs.NewValue == true)
+            {
+                uiElement.MouseMove += UIElementOnMouseMove;
+            }
+            else
+            {
+                uiElement.MouseMove -= UIElementOnMouseMove;
+            }
+
+        }
+        private static void UIElementOnMouseMove(object sender, MouseEventArgs mouseEventArgs)
+        {
+            var uiElement = sender as UIElement;
+            if (uiElement != null)
+            {
+                if (mouseEventArgs.LeftButton == MouseButtonState.Pressed)
+                {
+                    DependencyObject parent = uiElement;
+                    int avoidInfiniteLoop = 0;
+                    while ((parent is Window) == false)
+                    {
+                        parent = VisualTreeHelper.GetParent(parent);
+                        avoidInfiniteLoop++;
+                        if (avoidInfiniteLoop == 1000)
+                        {
+                            return;
+                        }
+                    }
+                    var window = parent as Window;
+                    window.DragMove();
+                }
+            }
+        }
+        public static void SetEnableDrag(DependencyObject element, bool value)
+        {
+            element.SetValue(EnableDragProperty, value);
+        }
+        public static bool GetEnableDrag(DependencyObject element)
+        {
+            return (bool)element.GetValue(EnableDragProperty);
+        }
+    }
+}
---
+++ ChibikaApp/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChibikaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double MinLeft = 0;
+        double MinTop = 0;
+
+        bool isGrounded = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var rightScreen = System.Windows.Forms.Screen.AllScreens[0];
+            var rightWorkingArea = rightScreen.WorkingArea;
+            MinTop = (rightWorkingArea.Bottom - Height);
+            MinLeft = rightWorkingArea.Left;
+            Top = MinTop;
+            Left = new Random().Next(100, 1500);
+            Debug.WriteLine("Top: " + Top);
+
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var randomWork = new Random();
+            this.Dispatcher.Invoke(() =>
+            {
+                    Task.Run(() =>
+                    {
+                        var nextWork = DateTime.Now + TimeSpan.FromSeconds(randomWork.Next(5, 15));
+
+                        Debug.WriteLine(nextWork.ToString());
+                        Debug.WriteLine(DateTime.Now);
+                        while (true)
+                        {
+                            if (DateTime.Now >= nextWork)
+                            {
+                                this.Dispatcher.Invoke(() =>
+                                {
+                                    if (this.Top == MinTop) isGrounded = true; else { isGrounded = false; }
+                                Debug.WriteLine(isGrounded);
+                                if (isGrounded)
+                                {
+                                    var randomWork_ = new Random();
+                                    int rw = randomWork_.Next(0, 3);
+                                    switch (rw)
+                                    {
+                                        case 0: // Nothing
+                                            Debug.WriteLine(rw);
+                                            break;
+                                        case 1:
+                                            Debug.WriteLine(rw);
+                                            moveTo(randomWork_.Next(0, 1500), 0);
+                                            break;
+                                        case 2:
+                                            Debug.WriteLine(rw);
+                                            jump();
+                                            break;
+                                    }
+                                }
+
+                                nextWork = DateTime.Now + TimeSpan.FromSeconds(randomWork.Next(5, 15));
+                                Debug.WriteLine(nextWork.ToString());
+
+                                });
+                            }
+                        }
+
+                        Task.Delay(250).Wait();
+                    });
+            });
+        }
+
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            monika.Source = new BitmapImage(new Uri("pack://application:,,,/Chibi/m_sticker_2.png"));
+            this.BeginAnimation(TopProperty, null);
+            this.DragMove();
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.BeginAnimation(LeftProperty, null);
+            monika.Source = new BitmapImage(new Uri("pack://application:,,,/Chibi/m_sticker_1.png"));
+            var rightScreen = System.Windows.Forms.Screen.AllScreens[0];
+            var rightWorkingArea = rightScreen.WorkingArea;
+
+            var falling = new DoubleAnimation();
+            falling.From = this.Top;
+            falling.To = MinTop;
+            falling.Duration = new Duration(TimeSpan.FromSeconds(1));
+
+            this.BeginAnimation(TopProperty, falling);
+        }
+        public void moveTo(int x, int y)
+        {
+            var falling = new DoubleAnimation();
+            falling.From = this.Left;
+            falling.To = x;
+            falling.Duration = new Duration(TimeSpan.FromSeconds(10));
+
+            this.BeginAnimation(LeftProperty, falling);
+        }
+        public void jump()
+        {
+            var falling = new DoubleAnimation();
+            falling.From = this.Top;
+            falling.To = (MinTop - new Random().Next(50,100));
+            falling.Duration = new Duration(TimeSpan.FromSeconds(0.5));
+
+            falling.Completed += (sender, args) =>
+            {
+                var falling1 = new DoubleAnimation();
+                falling1.From = this.Top;
+                falling1.To = MinTop;
+                falling1.Duration = new Duration(TimeSpan.FromSeconds(1));
+
+                this.BeginAnimation(TopProperty, falling1);
+            };
+            this.BeginAnimation(TopProperty, falling);
+        }
+
+    }
+}
